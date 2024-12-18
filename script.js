85ec87cc2cbd8f0804f6b4e4c927037480bf82d3
Elisei <eliseivlas.002@gmail.com>
2024-12-18 23:38:27 +0100

    corezione es

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 
 // Stabilisco un ciclo che mi stampi i numeri da 1 a 100
 for(let i=1; i<=100; i++){
+    let output;
+    
     //SE sono sia multipli di 3 e sia di 5 stampami FizzBuzz
     if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz")
+        output = "FizzBuzz";
     //Altrimenti se sono multipli di 5 stamba Bazz 
     }else if (i % 5 === 0){
-        console.log("Buzz")
+        output = "Buzz";
     //Altrimenti se sono multipli di 3 stampa Fizz
     }else if (i % 3 === 0){
-        console.log("Fizz")
+        output = "Fizz";
     //Altrimenti stampami il numero    
     }else{
-        console.log("numero " + i)
+        output = "numero " + i;
     }
+    console.log(output);
 }
 
     
